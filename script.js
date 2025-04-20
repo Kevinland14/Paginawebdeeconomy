c3a9cd0f2e2cce5b1c834c43add64ce1da019bd3
Kevinland14 <miprimerpaginaweb47@gmail.com>
2025-04-20 14:08:48 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function registrar(event) {
+  event.preventDefault();
+  const usuario = document.getElementById('registroUsuario').value;
+  const clave = document.getElementById('registroClave').value;
+  localStorage.setItem('usuario', usuario);
+  localStorage.setItem('clave', clave);
+  alert('¡Registrado correctamente!');
+  window.location.href = 'index.html';
+}
+
+function login(event) {
+  event.preventDefault();
+  const usuario = document.getElementById('loginUsuario').value;
+  const clave = document.getElementById('loginClave').value;
+  const usuarioGuardado = localStorage.getItem('usuario');
+  const claveGuardada = localStorage.getItem('clave');
+
+  if (usuario === usuarioGuardado && clave === claveGuardada) {
+    window.location.href = 'bienvenida.html';
+  } else {
+    alert('Usuario o clave incorrectos');
+  }
+}
